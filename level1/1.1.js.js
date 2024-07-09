69f2e98afa6bd3d08404a67698d658fe61921de9
Toshova Elena <maddin@list.ru>
2024-07-09 15:51:13 +0300

    First

---
+++ level1/1.1.js.js
@@ -0,0 +1,20 @@
+let num = -1;
+let message = (num<0) ? 'Число отрицательное':
+    (!num) ? 'Ноль':
+    'Число положительное';
+console.log(message)
+
+let line = 'jneeuffь';
+console.log(line.length)
+
+console.log(line[line.length-1])
+
+let even = (num%2==0)?'Четное':'Нечетное';
+console.log(even)
+
+
+let line2='jigririgrgr'
+
+line[0]==line2[0] ? console.log('Совпадают') : console.log('Не совпадают')
+
+line[line.length-1]=='ь' ? console.log(line[line.length-2]) : console.log(line[line.length-1])
